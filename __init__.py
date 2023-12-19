c94b6903c9abffad3bd8d34ed252e402d9ae2203
projeta618 <projeta618@gmail.com>
2023-12-19 12:36:26 -0300

    Add multiple corrective to the chartbubble

--- __init__.py
+++ __init__.py
@@ -1,3 +1,4 @@
 from .required import *
 from .mediacenter import * 
 from .token_manager import *
+from .signal import *
---
+++ signal.py
@@ -0,0 +1,8 @@
+from kernel.signal import SIGNAL_CENTER
+
+SIGNAL_CENTER.create(
+    'profile.load_me',
+    description="""
+        Fetch all the signal information of the user. 
+    """
+)
