2f299d85000a3a6b96794beed0c7cdfe893435a1
px57 <david@prepera.io>
2024-02-23 23:44:18 -0300

    feat: Add Welcome notification rule
    Why: Adding a new notification rule for welcoming users.
    
    Changes:
    - Added a new class "Notification" inheriting from "DefaultRuleClass"
    - Added a label "WELCOME" for the notification rule
    - Set the "allow" attribute to True

--- __interface__/notification.py
+++ __interface__/notification.py
@@ -3,3 +3,11 @@
 from notification.rules.stack import NOTIFICATION_RULESTACK
 from notification.rules.default_notification import DefaultRuleClass
 
+class Notification(DefaultRuleClass):
+    """
+    The default rule class. 
+    """
+
+    label = 'WELCOME'
+
+    allow = True
