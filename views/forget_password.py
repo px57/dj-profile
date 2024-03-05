4954fd0e4980d3b7b64ab21dd82733884000afcb
px57 <david@prepera.io>
2024-03-05 11:17:45 -0300

    Refactor: Update imports and function signatures in forget_password.py file
    The changes include:
    - Updated imports for load_response and switcher_send_message
    - Updated function signature for defineResetPasswordSessionValue
    - Updated function signature for forget_password
    - Removed unnecessary commented code
    - Updated usage of create_token and relatedModelId parameters
    - Added switcher_send_message call to send a message with token information

--- views/forget_password.py
+++ views/forget_password.py
@@ -1,6 +1,7 @@
 from kernel.http.decorators import load_json
 from kernel.http import Response
-from kernel.interfaces.decorators import load_interface
+from kernel.http import load_response 
+from kernel.message.centralize import switcher_send_message
 
 from django.views.decorators.csrf import csrf_exempt
 from django.conf import settings
@@ -8,15 +9,28 @@ from django.conf import settings
 from profiles.models import ResetPasswordModels, Profile
 from profiles.forms import ForgotPasswordForm
 
-from token_manager.libs import create_token, find_token
-from token_manager.models import TokenModels
+from token_manager.libs import create_token
+from token_manager.rules.stack import TOKEN_MANAGER_RULESTACK
+
+from profiles.__interface__.token_manager import TokenForgetPasswordRule
+from profiles.__interface__.mailling import MailForgetPassword
+from profiles.rules.stack import PROFILES_RULESTACK
 
 from uuid import uuid4
 
 
-def defineResetPasswordSessionValue(request, dbProfile, dBresetPasswordModels):
+def defineResetPasswordSessionValue(
+        request, 
+        dbProfile, 
+        dBresetPasswordModels
+    ):
     """
-        @description: Définir la sessions lors de la demande de mise à jours.
+    Définir la sessions lors de la demande de mise à jours.
+
+    Args:
+        request: La requête
+        dbProfile: Le profile
+        dBresetPasswordModels: Le model de reset password
     """
     serializedProfile = dbProfile.serialize(request)
 
@@ -31,22 +45,38 @@ def defineResetPasswordSessionValue(request, dbProfile, dBresetPasswordModels):
 
 @csrf_exempt
 @load_json
-# @load_interface(FORGET_PASSWORD_RULESTACK)
-def forget_password(request):
+@load_response(stack=PROFILES_RULESTACK)
+def forget_password(
+        request, 
+        res=None
+    ):
     """
-        @description: This function handles the forget password request
+    This function handles the forget password request
+
+    Args:
+        request: The request object
+        res: The response object
     """
-    res = Response(request=request)
     form = ForgotPasswordForm(request.POST)
 
     if not form.is_valid():
         return res.form_error(form)
     
     dbProfile: Profile = form.cleaned_data.get('email')
-    dbToken: TokenModels = create_token(
-        max_size=32,
-        relatedModel='profiles.Profile',
+    dbToken = create_token(
+        TOKEN_MANAGER_RULESTACK.get_rule(TokenForgetPasswordRule),
         relatedModelId=dbProfile.id
+    )
+    
+    switcher_send_message(
+        _inSwitch=MailForgetPassword,
+        res=res,
+        sendTo=dbProfile,
+        sendBy=dbProfile,
+        params={
+            'VERIFY_URL_TOKEN': dbToken.create_redirect_url(res),
+            'dbProfile': dbProfile
+        }
     )
 
     return res.success()
--- views/logout.py
+++ views/logout.py
@@ -1,6 +1,8 @@
 from django.contrib.auth import logout as django_logout
 from kernel.http import Response
 
+
+
 def logout(request):
     """Easy logout."""
     res = Response()
