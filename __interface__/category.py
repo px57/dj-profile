f3d0b3d4f88d74f00d6296b5db41be78c1d61679
px57 <david@prepera.io>
2024-03-07 22:52:41 -0300

    Refactor: Improve category.py file structure and add new methods for ProfileCategoryRule
    Why:
    - The category.py file needed refactoring to improve code readability and maintainability.
    - New methods were added to the ProfileCategoryRule class to enhance functionality related to category handling.
    
    Changes:
    - Added a new class attribute CATEGORY_CACHE for caching categories.
    - Added methods relatedModel__for__set_selected_category and relatedModelId__for__set_selected_category to provide information about the related model for selected categories.

--- __interface__/category.py
+++ __interface__/category.py
@@ -2,12 +2,40 @@
 from category.__rules__.stack import CATEGORY_RULESTACK
 from category.__rules__.default_category import DefaultRuleClass
 
-class DefaultRuleClass(DefaultRuleClass):
+CATEGORY_CACHE = {}
+
+class ProfileCategoryRule(DefaultRuleClass):
     """
     The default rule class. 
     """
 
     label = 'PROFILE:CATEGORY'
 
+    def relatedModel__for__set_selected_category(self):
+        """
+        Return the related model for the set selected category
+        """
+        from profiles.models import Profile
+        return Profile.__module__ + '.' + Profile.__name__
 
-CATEGORY_RULESTACK.set_rule(DefaultRuleClass)
+    def relatedModelId__for__set_selected_category(self):
+        """
+        Return the related model id for the set selected category
+        """
+        return self.request.profile.id
+    
+    def relatedModelId__for__get_related_categories(self):
+        """
+        Return the related model id for the get related categories
+        """
+        return self.relatedModelId__for__set_selected_category()
+
+    def event_set_selected_categories(self, dbSelectedCategories) -> bool:
+        '''
+        Set selected category is database save.
+        '''
+        print (dbSelectedCategories)
+        return True
+    
+
+CATEGORY_RULESTACK.set_rule(ProfileCategoryRule)
