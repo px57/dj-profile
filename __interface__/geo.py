1eaca8e8437760685b60c2b1574b3a65318b8820
px57 <david@prepera.io>
2024-03-13 12:58:51 -0300

    feat: add selectable country codes to SelectCountry class
    Why: To enhance the functionality of the SelectCountry rule by adding a list of selectable country codes.
    
    Changes:
    - Imported pycountry module for country code reference
    - Added list of selectable country codes including US, GB, FR, and AE to SelectCountry class

--- __interface__/geo.py
+++ __interface__/geo.py
@@ -2,10 +2,23 @@
 from geo.rules.stack import GEO_RULESTACK
 from geo.rules.default_geo import DefaultRuleClass
 
+import pycountry
+
 class SelectCountry(DefaultRuleClass):
     """
     This rule is used to select the country.
     """
     label = 'PROFILE:GEO'
+
+    selecteable_countries_code_list = [
+        # united states
+        'US',
+        # united kingdom
+        'GB',
+        # france
+        'FR',
+        # United Arab Emirates
+        'AE',
+    ]
 
 GEO_RULESTACK.add_rule(SelectCountry)
